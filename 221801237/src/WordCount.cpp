a01e817296b8e6feb7e53d834bf619269d657a0c
13055298738 <1205101977@qq.com>
2021-03-05 16:21:25 +0800

    先定义输入字符等计数的结构

--- 221801237/src/WordCount.cpp
+++ 221801237/src/WordCount.cpp
@@ -1,28 +1,20 @@
-#include"Lib.h"
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+
+
 using namespace std;
-int number_of_words=0;//记录单词统计总数
-vector<string> raw_word;//文本中的单词集
-vector<word> word_statistics;//统计结果集
-        
-int main(){
-	/*初始化时间*/
-   clock_t start,finish;
-   double totaltime;
-   start=clock();
+#define MAX_LENGTH 1<<10//单词的最大长度
 
 
-   printf("Wait for a moment please.\n");
-
-
-   /*定义缓存空间*/
-   word w;
-   char _word[MAX_LENGTH];
-
-
-   vector<string>::iterator it,last;
-   vector<word>::iterator wit;
-	wordCount();
-	characterCount();
-	lineCount();
-	outPut();
-}
+typedef struct{
+    string str;
+    int wordcnt;
+    int charcnt;
+    int linecnt;
+}word;
